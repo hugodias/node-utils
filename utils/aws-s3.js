33392f56e734b5bf08f157f7d3d04ca24a045fe3
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2019-01-17 21:45:38 -0200

    Simplify aws-s3:uploadBase64 function

--- utils/aws-s3.js
+++ utils/aws-s3.js
@@ -35,28 +35,14 @@ export async function uploadBase64(key, base64) {
   const fileType = base64.split(";")[0].split("/")[1];
   const filename = `${key}.${fileType}`;
 
-  return new Promise(async(resolve, reject) => {
-    try {
-      const data = await s3
-        .upload({
-          Bucket: process.env.AWS3_BUCKET_NAME,
-          Key: filename,
-          Body: base64Data,
-          ContentEncoding: "base64",
-          ContentType: `image/${fileType}`,
-          ACL: "public-read"
-        })
-        .promise();
-
-      logger.info(`aws-s3:uploadBase64 saved: ${data.Location}`);
-
-      resolve({
-        url: data.Location,
-        key: data.Key
-      });
-    } catch (err) {
-      logger.info(`aws-s3:uploadBase64 error: ${err}`);
-      reject(err);
-    }
-  });
+  return s3
+    .upload({
+      Bucket: process.env.AWS3_BUCKET_NAME,
+      Key: filename,
+      Body: base64Data,
+      ContentEncoding: "base64",
+      ContentType: `image/${fileType}`,
+      ACL: "public-read"
+    })
+    .promise();
 }
